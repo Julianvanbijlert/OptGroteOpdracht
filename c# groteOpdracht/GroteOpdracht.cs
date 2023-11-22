d59686060c7ed92bb5ea572f99b0285483963ad5
Julianvanbijlert <116498773+Julianvanbijlert@users.noreply.github.com>
2023-11-22 10:38:51 +0100

    c# files aangemaakt

---
+++ c# groteOpdracht/GroteOpdracht.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+//Manier om data in te lezen van
+    //Orderbestand  
+    //AfstandMatrix
+
+//Manier om solution te vinden
+
+//Manier om solutions op te slaan
+
+//Manier om solutions te representeren
+
